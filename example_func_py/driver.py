fb0def4ace3550fd8916a2a9bd38f941cb8765dd
wangliang <wangliang123@gmail.com>
2019-10-04 16:49:28 -0400

    up

---
+++ example_func_py/driver.py
@@ -0,0 +1,55 @@
+import sys
+sys.path.insert(0, "/var/task/")
+# del sys.modules["socket"]
+# del sys.modules["ssl"]
+
+if "ssl" in sys.modules:
+	del sys.modules["ssl"]
+
+# import socket1 as socket
+# 
+import func
+import json
+import urllib2
+import subprocess
+import json
+import time
+import boto3
+import botocore
+from conf import *
+
+import os
+import errno
+
+# from reader import monitor, listener
+
+import threading
+
+import zmq
+
+zmq_context = zmq.Context()
+
+
+def remove_tag(event):
+	tag = ""
+	body = event
+	if "tag" in event:
+		tag = event["tag"]
+		body = event["body"]
+	return tag, body
+
+
+def add_tag(event, dur=None):
+	if dur:
+		return {"tag": os.environ['AWS_LAMBDA_FUNCTION_NAME'], "body":event, "dur": dur}
+	return {"tag": os.environ['AWS_LAMBDA_FUNCTION_NAME'], "body":event}
+
+
+def lambda_handler(event, context):
+	event = {"test": "hello"}
+	res = func.main(event, context)
+	return res
+
+
+if __name__ == '__main__':
+    lambda_handler({"test":1}, None)
---
+++ example_func_py/func.py
@@ -0,0 +1,29 @@
+import json
+import urllib2
+import subprocess
+import json
+import time
+import boto3
+import zmq
+import socket
+from conf import *
+
+zmq_context = zmq.Context()
+
+def send_event(event_name, data):
+
+	# sk = zmq_context.socket(zmq.REQ)
+	# sk.connect("tcp://%s:%s" % (GUARD_IP, GUARD_PORT))
+	# sk.send("%s:%s" % (event_name, data))
+
+	sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sk.connect((GUARD_IP, GUARD_PORT))
+	sk.send("%s:%s" % (event_name, data))
+
+	resp = sk.recv(1024)
+	return resp
+
+def main(event, context):
+	response = urllib2.urlopen('https://www.cs.wisc.edu/', data=None)
+	response = urllib2.urlopen('https://www.wisc.edu/', data=None)
+	response = urllib2.urlopen('https://www.google.com/', data=None)
